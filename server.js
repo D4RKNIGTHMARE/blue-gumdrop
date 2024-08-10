f47ad79a85802b34ad9a90ee80513f5526c0d6b5
D4RKNIGTHMARE <130707667+D4RKNIGTHMARE@users.noreply.github.com>
2024-08-09 21:36:16 -0500

    Empezando el contenido de blue gumdrop

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src/views/index.html'));
+});
+
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/contacto.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log('Servidor corriendo en el puerto ${3000}')
+});
